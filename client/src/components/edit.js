6a54c9a3877a5deb5804ab09088da29884f9b217
Ryan Yang <peteryang666666@gmail.com>
2022-05-08 21:39:42 +0100

    Initial commit

---
+++ client/src/components/edit.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+
+export default function Edit() {
+  const [form, setForm] = useState({
+    ID: 0,
+    intro: "",
+  });
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      const id = params.id.toString();
+      const response = await fetch(`http://localhost:5000/record/${params.id.toString()}`);
+
+      if (!response.ok) {
+        const message = `An error has occured: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const record = await response.json();
+      if (!record) {
+        window.alert(`Record with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+
+      setForm(record);
+    }
+
+    fetchData();
+
+    return;
+  }, [params.id, navigate]);
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const editedPerson = {
+      ID: form.ID,
+      intro: form.intro,
+    };
+
+    // This will send a post request to update the data in the database.
+    await fetch(`http://localhost:5000/update/${params.id}`, {
+      method: "POST",
+      body: JSON.stringify(editedPerson),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    navigate("/");
+  }
+
+  // This following section will display the form that takes input from the user to update the data.
+  return (
+    <div>
+      <h3>Update Record</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="ID">ID: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="ID"
+            value={form.ID}
+            onChange={(e) => updateForm({ ID: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="intro">intro: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="intro"
+            value={form.intro}
+            onChange={(e) => updateForm({ intro: e.target.value })}
+          />
+        </div>
+        
+        <br />
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Update Record"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
